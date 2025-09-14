c93e5fa2f206df1ca53b1e97ca34dd7f89066426
Eno <enoyoo02@gmail.com>
2025-09-14 15:33:55 -0400

    safe area

--- Layouts/ContentView.swift
+++ Layouts/ContentView.swift
@@ -12,56 +12,59 @@ struct ContentView: View {
     @State private var messageString = "When the Genius Bar Needs Help, They Call YOU!"
     
     var body: some View {
-
-            VStack {
-                Text("You Have Skills!")
-                    .font(.largeTitle)
-                    .fontWeight(.black)
-                    .foregroundStyle(.goldBC)
-                    .padding()
-                    .background(.maroonBC)
-                    .clipShape(RoundedRectangle(cornerRadius: 15))
-                
-                
-                Spacer()
-                
-                Text(messageString)
-                    .font(.largeTitle)
-                    .fontWeight(.heavy)
-                    .minimumScaleFactor(0.5)
-                    .multilineTextAlignment(.center)
-                    .foregroundStyle(.maroonBC)
-                    .padding()
-                    .border(.orange)
-                    .frame(height: 150)
-                    .frame(maxWidth: .infinity)
-                    .border(.orange)
-                
-                Spacer()
-                
-                HStack {
-                    Button("Awesome") {
-                        messageString = "You Are Awesome"
-                    }
-                    
-                    Button("Great") {
-                        messageString = "You Are Great"
-                    }
-                    
-                }
-                .buttonStyle(.borderedProminent)
-                .tint(.maroonBC)
+        
+        VStack {
+            Text("You Have Skills!")
+                .font(.largeTitle)
+                .fontWeight(.black)
                 .foregroundStyle(.goldBC)
+                .padding()
+                .frame(maxWidth: .infinity)
+                .background(.maroonBC)
+                .clipShape(RoundedRectangle(cornerRadius: 15)) // REMOVE FOR SAFEAREA
+            
+            Spacer()
+            
+            Text(messageString)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .minimumScaleFactor(0.5)
+                .multilineTextAlignment(.center)
+                .foregroundStyle(.maroonBC)
+                .padding()
+                .frame(height: 150)
+                .frame(maxWidth: .infinity)
+            
+            Spacer()
+            
+            HStack {
+                Button("Awesome") {
+                    messageString = "You Are Awesome"
+                }
+                
+                Button("Great") {
+                    messageString = "You Are Great"
+                }
                 
             }
-        
+            .buttonStyle(.borderedProminent)
+            .tint(.maroonBC)
+            .foregroundStyle(.goldBC)
             .padding()
-            .background(
-                LinearGradient(
-                    colors: [.goldBC, .maroonBC],
-                    startPoint: .topLeading,
-                    endPoint: .bottomTrailing)
-            )
+            
+//            Rectangle()
+//                .frame(height: 0)
+//                .background(.goldBC)
+            
+        }
+        .padding() // REMOVE FOR SAFE SPOTS
+
+        .background(
+            LinearGradient(
+                colors: [.goldBC, .maroonBC],
+                startPoint: .topLeading,
+                endPoint: .bottomTrailing)
+        )
     }
 }
 
